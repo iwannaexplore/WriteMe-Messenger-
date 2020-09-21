06cc46e18f05598bc214b6c03feac7250515c4ae
dima <iwannaexplore@gmail.com>
2020-09-21 18:29:00 +0300

    Congifured chat and related pages

--- WriteMe/Controllers/HomeController.cs
+++ WriteMe/Controllers/HomeController.cs
@@ -1,5 +1,14 @@
-﻿using System.Diagnostics;
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis;
+using Microsoft.EntityFrameworkCore;
 using NLog;
 using WriteMe.Data;
 using WriteMe.Data.Entities;
@@ -9,36 +18,76 @@ using WriteMe.Models;
 
 namespace WriteMe.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
 
-        private readonly IGenericRepository<User> _userRepository;
-        private readonly IGenericRepository<Chat> _chatRepository;
-        private readonly IGenericRepository<FriendsList> _friendsListRepository;
-        private readonly IGenericRepository<FriendsRelationship> _friendsRelationshipRepository;
-        private readonly IGenericRepository<Message> _messageRepository;
+        private readonly DbSet<User> _userRepository;
+        private readonly DbSet<Chat> _chatRepository;
+        private readonly DbSet<FriendsRelationship> _friendsRelationshipRepository;
+        private readonly DbSet<Message> _messageRepository;
+        private readonly DbSet<FriendList> _friendListRepository;
+        private readonly ApplicationDbContext _context;
+        private  User CurrentUser => _context.Users.First(u => u.UserName == User.Identity.Name);
 
 
         public HomeController(ApplicationDbContext context)
         {
-            _userRepository = new GenericRepository<User>(context);
-            _chatRepository = new GenericRepository<Chat>(context);
-            _friendsListRepository = new GenericRepository<FriendsList>(context);
-            _friendsRelationshipRepository = new GenericRepository<FriendsRelationship>(context);
-            _messageRepository = new GenericRepository<Message>(context);
+            _context = context;
+            _userRepository = _context.Users;
+            _chatRepository = _context.Chats;
+            _friendsRelationshipRepository = _context.FriendsRelationships;
+            _messageRepository = _context.Messages;
+            _friendListRepository = _context.FriendLists;
         }
+
 
         public IActionResult Index()
         {
-            var z = _userRepository.GetAll();
-            return View();
+            var friendList = _friendListRepository.Include(fl => fl.FriendListUsers)
+                .Where(fl =>
+                    fl.FriendListUsers.Any(flu => flu.UserId == CurrentUser.Id) && fl.FriendsRelationshipId == 1)
+                .ToList();
+            List<User> users = new List<User>();
+            foreach (var list in friendList)
+            {
+                users.AddRange(_userRepository.Include(u => u.FriendListUsers)
+                    .Where(u => u.FriendListUsers.Any(flu => flu.FriendListId == list.Id) && u.Id != CurrentUser.Id));
+            }
+
+
+            return View(new Blabla() {Users = users});
         }
+
+        public IActionResult DisplayMessages(string from, int to)
+        {
+            Chat currentChat = _chatRepository.Include(c => c.FriendList).ThenInclude(fl => fl.FriendListUsers)
+                .Include(c => c.Messages)
+                .FirstOrDefault(c => c.FriendList.FriendListUsers.Any(flu => flu.UserId == to) && c.FriendList.FriendListUsers.Any(flu => flu.UserId == CurrentUser.Id));
+
+            var messages = _messageRepository.Where(m => m.ChatId == currentChat.ChatId).ToList();
+
+            return PartialView("_ChatRoom",
+                new HelpMeGod() {Messages = messages, Friend = _userRepository.First(u=>u.Id == to)});
+        }
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel {RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier});
         }
+    }
+
+    public class Blabla
+    {
+        public List<User> Users { get; set; }
+    }
+
+    public class HelpMeGod
+    {
+        public List<Message> Messages { get; set; }
+        public User Friend { get; set; }
     }
 }
